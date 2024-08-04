4ce90802f5998641cdf08357ea20dd7e57509e5b
“shijia” <shijia@tal.com>
2024-08-04 15:20:16 +0800

    有序链表合并

---
+++ leetcode_merge_21.py
@@ -0,0 +1,45 @@
+"""
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        pre = ListNode(-1)  # 头指针
+        head = pre
+        while list1 and list2:
+            if list1.val < list2.val:
+                pre.next = list1
+                list1 = list1.next
+            else:
+                pre.next = list2
+                list2 = list2.next
+            pre = pre.next
+        pre.next = list1 if list1 is not None else list2
+        return head.next
+
+
+if __name__ == '__main__':
+    l1 = ListNode(1, ListNode(2, ListNode(4)))
+    l2 = ListNode(1, ListNode(3, ListNode(4)))
+    solution = Solution()
+    res = solution.mergeTwoLists(l1, l2)
+    while res:
+        print(res.val)
+        res = res.next
